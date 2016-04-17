c1ae66629e3cf0736e0df34b98b2de5dc186c15c
Corey Robison <coreybrobison@gmail.com>
2016-04-17 17:49:07 -0600

    materialize

---
+++ app/homepage/homepage.controller.js
@@ -0,0 +1,8 @@
+angular.module("app").controller('homepageCtrl', function($scope, homepageService) {
+    
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+});
---
+++ app/homepage/homepage.service.js
@@ -0,0 +1,8 @@
+angular.module("app").service("homepageService", function() {
+
+    var homepage = {
+        name: 'homepage',
+        state: 'homepage'
+    }
+
+});
