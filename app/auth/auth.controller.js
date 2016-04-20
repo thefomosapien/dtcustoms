a8d67f751d27baa7123e8e6132611eb6c2ab0d29
Corey Robison <coreybrobison@gmail.com>
2016-04-20 17:37:32 -0600

    auth

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,6 +1,12 @@
 angular.module("app").controller('authCtrl', function ($scope) {
 
 
+    var ref = ('https://dtcustoms.firebaseio.com/');
+    var auth = new Firebase(ref);
+    var usersRef = new Firebase(ref + 'users/');
+    var users = $firebaseArray(usersRef);
+
+
     // Create a callback which logs the current auth state
     function authDataCallback(authData) {
         if (authData) {
@@ -9,12 +15,18 @@ angular.module("app").controller('authCtrl', function ($scope) {
             console.log("User is logged out");
         }
     }
-
     // Register the callback to be fired every time auth state changes
     var ref = new Firebase("https://dtcustoms.firebaseio.com");
     ref.onAuth(authDataCallback);
 
 
 
+
+
+
+
+    $('.message a').click(function () {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+    });
 
 });
