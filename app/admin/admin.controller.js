09529e4ca86e07cdf3489216920fdd46f905b774
Corey Robison <coreybrobison@gmail.com>
2016-04-21 22:59:52 -0600

    delete function for admin page

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -4,11 +4,15 @@ angular.module('app').controller('adminCtrl', function ($scope, $firebaseArray, 
     var baseUrl = "https://dtcustoms.firebaseio.com";
     var formInstance = new Firebase(baseUrl + "/messages");
     var logout = new Firebase(baseUrl)
-    
+
     $scope.messages = $firebaseArray(formInstance);
 
-    var logoutFunc = function($scope) {
-        logout.$unauth();
-    }
+    $scope.deleteAll = function() {
+    var deleteRef = new Firebase('https://dtcustoms.firebaseio.com/messages');
 
+    deleteRef.remove();
+    // All data at the database location for user 'fred' has been deleted
+    // (including any child data)
+
+    };
 }); 
